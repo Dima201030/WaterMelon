4501aea9e3afcb0838ccf6567b787005e89e4faf
dima <115942204+Dima201030@users.noreply.github.com>
2025-01-29 20:10:23 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,34 +1,16 @@
 #include <iostream>
 
 using namespace std;
-bool isInteger(const string &str) {
-    if (str.empty()) return false; // Пустая строка — не число
-
-    for (char ch : str) {
-        if (isdigit(ch)) return false; // Если не цифра — не число
-    }
-
-    return true;
-}
-
 int main() {
     setlocale(LC_ALL, "RU");
-    string str;
-    int leight;
-    
+    int number;
     while (true) {
-        cin >> str;leight = str.length();
-        if (isInteger(str)) { 
-            if (str.length() > 10) {
-                cout << str[0] << leight - 2 << str[str.length() - 1] << endl;
-            } else {
-                cout << str << endl;
-            }
+        cin >> number;
+        if (number % 2 == 0) {
+            cout << "YES" << endl;
         } else {
-            str = "";
-            cout << "Error: an integer has been entered! The variable has been reset to zero.\n";
+            cout << "NO" << endl;
         }
-        
     }
     return 0;
 }
